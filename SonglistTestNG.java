f33ed9786ec12cacb5a348c1b17f67015086fc17
suhas2656 <51942573+suhas2656@users.noreply.github.com>
2023-09-13 15:12:54 +0530

    Create SonglistTestNG.java
    Added Test file

---
+++ SonglistTestNG.java
@@ -0,0 +1,63 @@
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SonglistTestNG {
+
+    /**
+     * Test case to verify the functionality of adding songs to a user's recently played playlist.
+     * It checks if songs are added correctly, limited to a specified maximum count,
+     * and if the most recent songs are at the end of the playlist.
+     */
+    @Test
+    public void testAddingSongs() {
+        RecentlyPlayedSongs store = new RecentlyPlayedSongs(3);
+
+        // Add songs for User1
+        store.addSong("User1", "Song 1");
+        store.addSong("User1", "Song 2");
+        store.addSong("User1", "Song 3");
+
+        LinkedList<String> user1RecentlyPlayed = store.getRecentlyPlayed("User1");
+
+        // Check if User1's recently played songs match the expected songs
+        Assert.assertEquals(user1RecentlyPlayed, List.of("Song 1", "Song 2", "Song 3"));
+
+        // Add more songs for User1
+        store.addSong("User1", "Song 4");
+        store.addSong("User1", "Song 5");
+
+        user1RecentlyPlayed = store.getRecentlyPlayed("User1");
+
+        // Check if User1's playlist still has a maximum of 3 songs
+        Assert.assertEquals(user1RecentlyPlayed.size(), 3);
+
+        // Check if the most recent songs are at the end of the playlist
+        Assert.assertEquals(user1RecentlyPlayed, List.of("Song 3", "Song 4", "Song 5"));
+    }
+
+    /**
+     * Test case to verify the functionality of adding songs for different users
+     * and ensuring that each user's playlist contains their respective songs.
+     */
+    @Test
+    public void testAddingSongsForDifferentUsers() {
+        RecentlyPlayedSongs store = new RecentlyPlayedSongs(3);
+
+        // Add songs for User1 and User2
+        store.addSong("User1", "Song 1");
+        store.addSong("User2", "Song A");
+        store.addSong("User1", "Song 2");
+        store.addSong("User2", "Song B");
+        store.addSong("User1", "Song 3");
+
+        LinkedList<String> user1RecentlyPlayed = store.getRecentlyPlayed("User1");
+        LinkedList<String> user2RecentlyPlayed = store.getRecentlyPlayed("User2");
+
+        // Check if each user's playlist contains their respective songs
+        Assert.assertEquals(user1RecentlyPlayed, List.of("Song 1", "Song 2", "Song 3"));
+        Assert.assertEquals(user2RecentlyPlayed, List.of("Song A", "Song B"));
+    }
+}
